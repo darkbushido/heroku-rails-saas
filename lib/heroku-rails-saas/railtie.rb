f2e564d6ef810b49b6f88b3deb2859da7113ba4c
Chris Trinh <chris.chtrinh@gmail.com>
2013-03-21 16:21:35 -0700

    Load rake task only if environment is not test

--- lib/heroku-rails-saas/railtie.rb
+++ lib/heroku-rails-saas/railtie.rb
@@ -5,7 +5,7 @@ module HerokuRailsSaas
   class Railtie < ::Rails::Railtie
     rake_tasks do
       HerokuRailsSaas::Config.root = ::Rails.root
-      load 'heroku/rails/tasks.rb'
+      load 'heroku/rails/tasks.rb' unless ::Rails.env.test?
     end
   end
 end
